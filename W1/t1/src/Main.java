ad8c39ed424b729e7078347cf171eb30ba1f79c0
esraamjdy <esraamjdy@gmail.com>
2024-04-19 23:28:29 +0200

    weeks tasks

---
+++ W1/t1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int age=21;
+        double height=111;
+        boolean raining=true;
+        String city="Cairo";
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(raining);
+        System.out.println(city);
+    }
+}
